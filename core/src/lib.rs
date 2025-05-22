d7b2782e6794622b1cf72bae7b41853d329e867f
athlwulf <99441880+athlwulf@users.noreply.github.com>
2025-05-22 13:10:40 -0700

    Update lib.rs
    Added to expand use of models

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod models;
+pub mod csv_processing;
+pub mod persistence;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use models::{
+    Employee, Skill, GridBox, GridState, AppSettings
+};
+pub use csv_processing::{
+    import_employees_from_csv, export_employees_to_csv
+};
+pub use persistence::{
+    save_employee_note, load_employee_note, save_app_settings, load_app_settings
+};
